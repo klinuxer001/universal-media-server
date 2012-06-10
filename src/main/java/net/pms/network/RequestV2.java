ee83ca758ece9a819518a78897427f5289a7b037
SubJunk <subjunk@gmail.com>
2012-06-10 12:12:02 +1200

    Formatting

--- src/main/java/net/pms/network/RequestV2.java
+++ src/main/java/net/pms/network/RequestV2.java
@@ -697,38 +697,37 @@ public class RequestV2 extends HTTPResource {
 		} else if (method.equals("SUBSCRIBE")) {
 			output.setHeader("SID", PMS.get().usn());
 			output.setHeader("TIMEOUT", "Second-1800");
-			String cb=soapaction.replace("<", "").replace(">", "");
-			String faddr=cb.replace("http://", "").replace("/", "");
-			String addr=faddr.split(":")[0];
-			int port=Integer.parseInt(faddr.split(":")[1]);
-			Socket sock=new Socket(addr,port);
-			OutputStream out=sock.getOutputStream();
-			out.write(("NOTIFY /"+argument+" HTTP/1.1").getBytes());
+			String cb = soapaction.replace("<", "").replace(">", "");
+			String faddr = cb.replace("http://", "").replace("/", "");
+			String addr = faddr.split(":")[0];
+			int port = Integer.parseInt(faddr.split(":")[1]);
+			Socket sock = new Socket(addr,port);
+			OutputStream out = sock.getOutputStream();
+			out.write(("NOTIFY /" + argument + " HTTP/1.1").getBytes());
 			out.write(CRLF.getBytes());
-			out.write(("SID: "+PMS.get().usn()).getBytes());
+			out.write(("SID: " + PMS.get().usn()).getBytes());
 			out.write(CRLF.getBytes());
-			out.write(("SEQ: "+0).getBytes());
+			out.write(("SEQ: " + 0).getBytes());
 			out.write(CRLF.getBytes());
 			out.write(("NT: upnp:event").getBytes());
 			out.write(CRLF.getBytes());
 			out.write(("NTS: upnp:propchange").getBytes());
 			out.write(CRLF.getBytes());
-			out.write(("HOST: "+faddr).getBytes());
+			out.write(("HOST: " + faddr).getBytes());
 			out.write(CRLF.getBytes());
 			out.flush();
 			out.close();
-			if(argument.contains("connection_manager")) {
+			if (argument.contains("connection_manager")) {
 				response.append(HTTPXMLHelper.eventHeader("urn:schemas-upnp-org:service:ConnectionManager:1"));
 				response.append(HTTPXMLHelper.eventProp("SinkProtocolInfo"));
 				response.append(HTTPXMLHelper.eventProp("SourceProtocolInfo"));
 				response.append(HTTPXMLHelper.eventProp("CurrentConnectionIDs"));
 				response.append(HTTPXMLHelper.EVENT_FOOTER);
-			}
-			else if(argument.contains("content_directory")) {
+			} else if (argument.contains("content_directory")) {
 				response.append(HTTPXMLHelper.eventHeader("urn:schemas-upnp-org:service:ContentDirectory:1"));
 				response.append(HTTPXMLHelper.eventProp("TransferIDs"));
 				response.append(HTTPXMLHelper.eventProp("ContainerUpdateIDs"));
-				response.append(HTTPXMLHelper.eventProp("SystemUpdateID",""+DLNAResource.getSystemUpdateId()));
+				response.append(HTTPXMLHelper.eventProp("SystemUpdateID", "" + DLNAResource.getSystemUpdateId()));
 				response.append(HTTPXMLHelper.EVENT_FOOTER);
 			}
 		} else if (method.equals("NOTIFY")) {
